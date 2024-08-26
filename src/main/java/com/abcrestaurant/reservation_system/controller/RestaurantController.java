6de6fc4c3bf950f7bf5e0ba53b31bec923b5fc7f
Shehanford <shehanrford@gmail.com>
2024-08-26 20:19:43 +0530

    Backend - Create Restaurant Service and Controller

---
+++ src/main/java/com/abcrestaurant/reservation_system/controller/RestaurantController.java
@@ -0,0 +1,41 @@
+package com.abcrestaurant.reservation_system.controller;
+
+import com.abcrestaurant.reservation_system.model.Restaurant;
+import com.abcrestaurant.reservation_system.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/restaurants")
+public class RestaurantController {
+    @Autowired
+    private RestaurantService restaurantService;
+
+    @PostMapping("/")
+    public ResponseEntity<Restaurant> createRestaurant(@RequestBody Restaurant restaurant) {
+        Restaurant createdRestaurant = restaurantService.createRestaurant(restaurant);
+        return ResponseEntity.ok(createdRestaurant);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Restaurant>> getAllRestaurants() {
+        List<Restaurant> restaurants = restaurantService.getAllRestaurants();
+        return ResponseEntity.ok(restaurants);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Restaurant> getRestaurantById(@PathVariable String id) {
+        return restaurantService.getRestaurantById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteRestaurant(@PathVariable String id) {
+        restaurantService.deleteRestaurant(id);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/abcrestaurant/reservation_system/service/RestaurantService.java
@@ -0,0 +1,5 @@
+package com.abcrestaurant.reservation_system.service;
+
+public class RestaurantService {
+
+}
